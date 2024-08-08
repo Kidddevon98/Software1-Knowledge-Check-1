d81c7174db56653164a25c06768c0771778b4a11
Devon Kidd <kidddevon98@gmail.com>
2024-08-08 17:19:23 -0400

    Resolved merge conflicts

--- obj/Debug/net8.0/KnowledgeCheck1_Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/KnowledgeCheck1_Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KnowledgeCheck1_Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bcd3fd4b82a047278285e46faa7753b7ec7a13e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42f942c90b0d7f02ed59894ce1ea514071cc5727")]
 [assembly: System.Reflection.AssemblyProductAttribute("KnowledgeCheck1_Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KnowledgeCheck1_Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
